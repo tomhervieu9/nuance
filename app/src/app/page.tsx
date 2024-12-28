af4db4d1372b6e5e4f6a2997ef577e9968576e17
Thomas <tom.hervieu@gmail.com>
2024-12-27 22:10:18 -0500

    add a simple client component like button

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import LikeButton from "./like-button";
 
 export default function Home() {
   return (
@@ -48,6 +49,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <LikeButton />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
