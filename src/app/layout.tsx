b67b3ec351b8f332fa1a61475172eca9870e4bb5
Thomas <tom.hervieu@gmail.com>
2024-12-28 19:58:31 -0500

    add default project root path alias

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./ui/global.css";
+import "@nuance/ui/global.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
