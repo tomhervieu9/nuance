26df42bf7323d34645dd5eb8f0786e8e8972509c
tom <tom.hervieu@gmail.com>
2025-05-04 21:08:03 -0400

    add basic DB query to 'assets' page

--- src/app/dashboard/assets/page.tsx
+++ src/app/dashboard/assets/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <p>Assets Page</p>;
+import { fetchUser1Balance } from "@/app/query/route";
+
+export default async function Page() {
+  const user1Balance = await fetchUser1Balance();
+  return (
+    <main>
+      <p>Assets Page</p>
+      <div> User 1 balance: ${user1Balance}</div>
+    </main>
+  );
 }
--- src/app/query/route.ts
+++ src/app/query/route.ts
@@ -1,4 +1,5 @@
 import postgres from "postgres";
+import { users } from "../lib/placeholder-data/dummyUsers";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -9,6 +10,19 @@ async function listUserBalances() {
   `;
 
   return data;
+}
+
+// Fetches the balance of User 1 from the database.
+export async function fetchUser1Balance() {
+  try {
+    const data = await sql<
+      { balance: number }[]
+    >`SELECT balance FROM users WHERE id = ${users[0].id}`;
+    return data[0].balance;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch User 1 balance.");
+  }
 }
 
 export async function GET() {
