f9ea7697da43736ea611a19cccf1558443288fe7
tom <tom.hervieu@gmail.com>
2025-05-04 21:40:56 -0400

    add comments

--- src/app/query/route.ts
+++ src/app/query/route.ts
@@ -3,6 +3,12 @@ import { users } from "../lib/placeholder-data/dummyUsers";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
+/**
+ * Note: To reduce the amount of data sent over each DB request, and also to reduce the amount
+ * of necessary compute on the frontend, narrow down the data fetched via SQL parameters (using LIMIT, MAX, etc.)
+ * as much as possible.
+ */
+
 async function listUserBalances() {
   const data = await sql`
     SELECT users.name, users.balance
@@ -18,6 +24,9 @@ export async function fetchUser1Balance() {
     const data = await sql<
       { balance: number }[]
     >`SELECT balance FROM users WHERE id = ${users[0].id}`;
+
+    // Add a delay to simulate a slow database response (for demonstration purposes only).
+    // await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulate a delay
     return data[0].balance;
   } catch (error) {
     console.error("Database Error:", error);
